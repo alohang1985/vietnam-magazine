5a2676a60a7ee7be28dc61384466c9b29cd3ca61
Youngdon Jang <youngdonjang@Youngdonui-Macmini.local>
2026-02-21 02:29:13 +0700

    fix: add types + pin node + clean next config

--- apps/frontend-next/next.config.js
+++ apps/frontend-next/next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true
-  },
+  
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'images.unsplash.com', pathname: '/**' },
